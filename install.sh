c80eb8d264aa1ff477507764bf218779ed81e6de
jason3e7 <jason3e7@gmail.com>
2019-03-15 14:45:59 +0800

    fix install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ git clone https://github.com/jason3e7/metasploit-framework.git
 cd metasploit-framework/
 git checkout develop
 (cd / && patch -t -p0) < $workpath/metasploit-framework/phpmyadmin_config.patch
-(cd / && patch -t -p0) < $workpath/metasploit-framework/cve-2018-14432.patch
+(cd / && patch -t -p0) < $workpath/metasploit-framework/cve_2018_14432.patch
