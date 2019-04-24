f8690a3a9adb84167f251f5c516f2f908879de31
jason3e7 <jason3e7@gmail.com>
2019-04-24 15:38:37 +0800

    add uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+(cd / && patch -t -p0) < ./phpmyadmin_config.patch
+(cd / && patch -t -p0) < ./cve_2018_14432.patch
+(cd / && patch -t -p0) < ./cve_2018_20170.patch
+(cd / && patch -t -p0) < ./cve_2017_4940.patch
